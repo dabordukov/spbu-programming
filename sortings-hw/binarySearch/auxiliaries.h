739f9743d554bdda3766967161f9030117e170d1
Dmitrii Bordukov <zst@localhost>
2024-10-04 18:53:59 +0300

    upload hw

---
+++ sortings-hw/binarySearch/auxiliaries.h
@@ -0,0 +1,9 @@
+#ifndef AUX
+#define AUX 1
+#include <stdbool.h>
+
+void swap(int* a, int* b);
+void printArray(int* array, int size);
+void fillArrayRand(int* array, int size, int rangeMax);
+
+#endif
