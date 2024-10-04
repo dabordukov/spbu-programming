0a09bee5a6947c787076b7a7012705e9822984f6
Dmitrii Bordukov <zst@localhost>
2024-10-04 18:42:46 +0300

    upload hw

---
+++ complexity-hw/fibonacciNumbers/fibonacciTests.c
@@ -0,0 +1,40 @@
+#include <assert.h>
+#include <stdbool.h>
+
+bool testCaseFibonacciFirst20(int (*fibonacci)(int, int*)) {
+    int precalculatedFibonacci[20] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+                                      89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765};
+
+    int result = 0, error = 0;
+    for (int i = 0; i < 20; i++) {
+        result = fibonacci(i + 1, &error);
+        if (result != precalculatedFibonacci[i] || error != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool testCaseFibonacciNonNatural(int (*fibonacci)(int, int*)) {
+    int error = 0;
+    fibonacci(0, &error);
+    if (error != -1) {
+        return false;
+    }
+
+    fibonacci(-1, &error);
+    return error == -1;
+}
+
+bool testCaseFibonacciOverflowReturnsError(int (*fibonacci)(int, int*)) {
+    int error = 0;
+    fibonacci(47, &error);
+    return error == 1;
+}
+
+// void fibonacciTestAll(int (*fibonacci)(int, int*)) {
+//     testCaseFibonacciFirst20(fibonacci);
+//     testCaseFibonacciNonNatural(fibonacci);
+//     testCaseFibonacciOverflowReturnsError(fibonacci);
+// }
