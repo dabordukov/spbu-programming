aac0b1a07aebf30aad92eaf507466bd286f56354
Dmitrii Bordukov <zst@localhost>
2024-10-17 20:08:00 +0300

    rewrited tests for new db structure

--- structuresModulesFiles/phoneBook/phoneBook/tests/phoneBookTest.c
+++ structuresModulesFiles/phoneBook/phoneBook/tests/phoneBookTest.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "../ioAuxiliaries.h"
 #include "../phoneBook.h"
 
 #define JOIN(x, y) x #y
@@ -10,75 +11,36 @@
 #define DB_LOAD_TEST_PATH JOIN(PHONEBOOK_TESTS_DIR, dbLoad.test)
 bool testLoadDatabase() {
     phoneBook book;
-    wchar_t names[][PHONEBOOK_NAME_LENGTH_MAX] = {L"qwertyuiop", L"йцукенгшщд"};
-    char numbers[][PHONEBOOK_NUMBER_LENGTH_MAX] = {"123456789", " 0 2"};
+    char names[][PHONEBOOK_NAME_LENGTH_MAX] = {"qwertyuiop", "тесттест тест"};
+    char numbers[][PHONEBOOK_NUMBER_LENGTH_MAX] = {"123456789", "+0000 222"};
 
-    loadPhoneBook(&book, DB_LOAD_TEST_PATH);
-
-    FILE* test = fopen(DB_LOAD_TEST_PATH, "r");
-    if (test == NULL) {
-        fprintf(stderr, "Error: Can't open %s for reading.\n", DB_LOAD_TEST_PATH);
+    if (loadPhoneBook(&book, DB_LOAD_TEST_PATH)) {
         return false;
     }
-    bool passed = true;
-
-    // compare bytes of file and structure
-    char byte[1];
-    for (int i = 0; i < sizeof(phoneBook); i++) {
-        if (fread(byte, 1, 1, test) != 1 || byte[0] != ((char*)&book)[i]) {
-            passed = false;
-            break;
-        }
-    }
-    // if the file's size is bigger than the structure's
-    if (fread(byte, 1, 1, test) != 0) {
-        passed = false;
-    }
-
-    fclose(test);
 
     for (int i = 0; i < book.size; i++) {
-        if (wcscmp(book.entries[i].name, names[i]) != 0 || strcmp(book.entries[i].number, numbers[i]) != 0) {
+        if (strncmp(book.entries[i].name, names[i], PHONEBOOK_NAME_LENGTH_MAX) != 0 || strncmp(book.entries[i].number, numbers[i], PHONEBOOK_NUMBER_LENGTH_MAX) != 0) {
             return false;
         }
     }
 
-    return passed;
+    return true;
 }
 
 #define DB_ADD_ENTRY_TEST_PATH JOIN(PHONEBOOK_TESTS_DIR, addEntry.test)
 bool testAddEntry() {
     phoneBook book;
-    loadPhoneBook(&book, DB_LOAD_TEST_PATH);
-
-    wchar_t name[] = L"тесттесттест1234";
+    // loadPhoneBook(&book, DB_LOAD_TEST_PATH);
+    char name[] = "тесттесттест1234";
     char number[] = "000000000000";
     phoneBookAddEntry(&book, name, number);
-    book.saved = true;
 
-    FILE* test = fopen(DB_ADD_ENTRY_TEST_PATH, "r");
-    if (test == NULL) {
-        fprintf(stderr, "Error: Can't open %s for reading.\n", DB_ADD_ENTRY_TEST_PATH);
+    if (strncmp(book.entries[book.size - 1].name, name, PHONEBOOK_NAME_LENGTH_MAX) != 0 ||
+        strncmp(book.entries[book.size - 1].number, number, PHONEBOOK_NUMBER_LENGTH_MAX) != 0) {
         return false;
     }
-    bool passed = true;
 
-    // compare bytes of file and structure
-    char byte[1];
-    for (int i = 0; i < sizeof(phoneBook); i++) {
-        if (fread(byte, 1, 1, test) != 1 || byte[0] != ((char*)&book)[i]) {
-            passed = false;
-            break;
-        }
-    }
-    // if the file's size is bigger than the structure's
-    if (fread(byte, 1, 1, test) != 0) {
-        passed = false;
-    }
-
-    fclose(test);
-
-    return passed;
+    return true;
 }
 
 #define DB_SAVE_TEST_PATH DB_ADD_ENTRY_TEST_PATH
@@ -88,40 +50,38 @@ bool testSaveDatabase() {
 
     loadPhoneBook(&book, DB_LOAD_TEST_PATH);
 
-    wchar_t name[] = L"тесттесттест1234";
+    char name[] = "тесттесттест1234";
     char number[] = "000000000000";
     phoneBookAddEntry(&book, name, number);
     savePhoneBook(&book, DB_SAVE_CURR_DB);
 
-    FILE* test = fopen(DB_SAVE_TEST_PATH, "r");
-    if (test == NULL) {
-        fprintf(stderr, "Error: Can't open %s for reading.\n", DB_SAVE_TEST_PATH);
-        return false;
-    }
     FILE* db = fopen(DB_SAVE_CURR_DB, "r");
-    if (test == NULL) {
+    if (db == NULL) {
         fprintf(stderr, "Error: Can't open %s for reading.\n", DB_SAVE_CURR_DB);
         return false;
     }
 
-    bool passed = true;
-    int db_char = 0;
-    int test_char = 0;
-    while (db_char != EOF && test_char != EOF) {
-        db_char = getc(db);
-        test_char = getc(test);
-        if (test_char != db_char) {
-            printf("%d, || %d \n", test_char, db_char);
-            passed = false;
-            break;
+    int size = 0;
+    if (fscanf(db, "%d\n", &size) != 1 || size != book.size) {
+        return false;
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (freadLineN(db, name, PHONEBOOK_NAME_LENGTH_MAX) == 0 ||
+            freadLineN(db, number, PHONEBOOK_NUMBER_LENGTH_MAX) == 0) {
+            return false;
+        }
+
+        if (strncmp(name, book.entries[i].name, PHONEBOOK_NAME_LENGTH_MAX) != 0 ||
+            strncmp(number, book.entries[i].number, PHONEBOOK_NUMBER_LENGTH_MAX) != 0) {
+            return false;
         }
     }
 
     fclose(db);
-    fclose(test);
     remove(DB_SAVE_CURR_DB);
 
-    return passed;
+    return true;
 }
 
 #define SEARCH_BY_NAME_TEST_PATH JOIN(PHONEBOOK_TESTS_DIR, nameSearch.test)
@@ -130,12 +90,12 @@ bool testSearchByName() {
     loadPhoneBook(&book, SEARCH_BY_NAME_TEST_PATH);
 
     int findResults[PHONEBOOK_MAX_ENTRIES];
-    int entriesCount = findSimilarNamesPhoneBook(&book, L"test", findResults);
+    int entriesCount = findSimilarNamesPhoneBook(&book, "тест", findResults);
     if (!(entriesCount == 3 && findResults[0] == 0 && findResults[1] == 1 && findResults[2] == 4)) {
         return false;
     }
 
-    entriesCount = findSimilarNamesPhoneBook(&book, L"test12345", findResults);
+    entriesCount = findSimilarNamesPhoneBook(&book, "test12345", findResults);
     if (!(entriesCount == 1 && findResults[0] == 3))
         return false;
 
@@ -159,8 +119,8 @@ bool testSearchByPhoneNumber() {
 int runTest() {
     assert(testLoadDatabase());
     assert(testAddEntry());
-    assert(testSearchByName());
     assert(testSaveDatabase());
+    assert(testSearchByName());
     assert(testSearchByPhoneNumber());
 
     return 0;
