a5972394efca3f07b9e266a261273644eb838f37
Dmitrii Bordukov <zst@localhost>
2024-10-25 00:20:39 +0300

    removed unnecessary files

--- test1/task1/tests.c
+++
@@ -1,6 +0,0 @@
-#include <stdbool.h>
-#include <wchar.h>
-
-void testEmptyPalindrome(bool (*isPalindrome)(wchar_t*, int)) {
-    char emptyPalindrome[1] = "";
-}
--- test1/task2/auxiliaries.h
+++
@@ -1,9 +0,0 @@
-#ifndef AUX
-#define AUX 1
-#include <stdbool.h>
-
-void swap(int* a, int* b);
-void printArray(int* array, int size);
-void fillArrayRand(int* array, int size, int rangeMax);
-
-#endif
