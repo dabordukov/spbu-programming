4bee03b59f0a25187d69bddc9d369761ff070740
Dmitrii Bordukov <zst@localhost>
2024-10-29 00:11:26 +0300

    Initial commit

---
+++ dynamicStructures/postfixCalculator/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include "stackOnPointers/stack.h"
+
+int main() {
+    ;
+}
---
+++ dynamicStructures/postfixCalculator/postfixCalculator.c
@@ -0,0 +1,5 @@
+#include "stackOnPointers/stack.h"
+
+int evaluateExpression(char* expresion, int* error) {
+    return 0;
+}
