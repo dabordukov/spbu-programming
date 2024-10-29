9fee52bfdd1c1ee75d6497a9e63edf50f4382ee2
Dmitrii Bordukov <zst@localhost>
2024-10-29 18:35:33 +0300

    added user interaction

--- dynamicStructures/postfixCalculator/main.c
+++ dynamicStructures/postfixCalculator/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-#include "stackOnPointers/stack.h"
+#include "ioAuxiliaries.h"
+#include "postfixCalculator.h"
 
 int main() {
-    ;
+    printf("Введите выражение в постфиксной форме:\n");
+    char* expression = NULL;
+    int error;
+    readLine(&expression, &error);
+    if (error != 0) {
+        fprintf(stderr, "Something went wrong...\n");
+        return 1;
+    }
+
+    int result = calculate(expression, &error);
+    if (error != 0) {
+        printf("Ошибка в выражении\n");
+    } else {
+        printf("Результат: %d\n", result);
+    }
 }
