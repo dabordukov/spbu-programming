e3785b981f913c9b323ec7c41a7ba1662cc00f3e
Dmitrii Bordukov <zst@localhost>
2024-10-30 22:36:16 +0300

    removed trash files

--- dynamicStructures/shuntingYard/stackOnPointers/build/CMakeFiles/testStack.dir/home/zst/spbu/programming/dynamicStructures/postfixCalculator/postfixCalculatorTokens.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/testStack.dir/home/zst/spbu/programming/dynamicStructures/postfixCalculator/postfixCalculatorTokens.c.o: \
- /home/zst/spbu/programming/dynamicStructures/postfixCalculator/postfixCalculatorTokens.c \
- /usr/include/stdc-predef.h \
- /home/zst/spbu/programming/dynamicStructures/postfixCalculator/postfixCalculatorTokens.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdbool.h
