fdaab0b74c9717e456e5070ea4aaaadea0720933
Dmitrii Bordukov <zst@localhost>
2024-10-30 22:42:20 +0300

    windows cmd codepage support

--- dynamicStructures/postfixCalculator/main.c
+++ dynamicStructures/postfixCalculator/main.c
@@ -1,9 +1,16 @@
+#include <locale.h>
 #include <stdio.h>
 
 #include "ioAuxiliaries.h"
 #include "postfixCalculator.h"
 
 int main() {
+#ifdef _WIN32
+    system("chcp 1251");
+    setlocale(LC_ALL, "Russian");
+#else
+    setlocale(LC_CTYPE, "");
+#endif
     printf("Введите выражение в постфиксной форме:\n");
     char* expression = NULL;
     int error;
