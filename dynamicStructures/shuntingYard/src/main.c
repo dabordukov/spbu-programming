30fec186cd9c6701324bc9bacff173cfe1a32ee2
Dmitrii Bordukov <zst@localhost>
2024-10-30 22:43:06 +0300

    windows cmd codepage support

--- dynamicStructures/shuntingYard/src/main.c
+++ dynamicStructures/shuntingYard/src/main.c
@@ -1,9 +1,15 @@
+#include <locale.h>
 #include <stdio.h>
 
 #include "ioAuxiliaries.h"
 #include "shuntingYard.h"
-
 int main() {
+#ifdef _WIN32
+    system("chcp 1251");
+    setlocale(LC_ALL, "Russian");
+#else
+    setlocale(LC_CTYPE, "");
+#endif
     char* out = NULL;
     char* in = NULL;
     int err = 0;
