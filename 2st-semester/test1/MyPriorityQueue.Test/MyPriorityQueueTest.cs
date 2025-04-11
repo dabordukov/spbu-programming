d73c9b677fbce54918f1a92fc8e8cb5d894bd6c6
Dmitrii Bordukov <dabordukov@mail.ru>
2025-04-11 15:01:35 +0300

    feat: add MyPriorityQueue implementation and corresponding tests

---
+++ 2st-semester/test1/MyPriorityQueue.Test/MyPriorityQueueTest.cs
@@ -0,0 +1,33 @@
+﻿namespace MyPriorityQueue.Test;
+
+public class MyPriorityQueueTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void MyPriorityQueueShouldEnqueueDequeueCorrectly()
+    {
+        MyPriorityQueue queue = new();
+        Assert.That(queue.Empty, Is.True);
+        queue.Enqueue(1, 1);
+        Assert.That(queue.Empty, Is.False);
+        Assert.That(queue.Dequeue(), Is.EqualTo(1));
+        Assert.That(queue.Empty, Is.True);
+    }
+
+    [Test]
+    public void MyPriorityQueueShouldDequeueIn_HigherPriorityFirst_Order()
+    {
+        MyPriorityQueue queue = new();
+
+        queue.Enqueue(2, 5);
+        queue.Enqueue(1, 8);
+        queue.Enqueue(3, -5);
+        Assert.That(queue.Dequeue(), Is.EqualTo(1));
+        Assert.That(queue.Dequeue(), Is.EqualTo(2));
+        Assert.That(queue.Dequeue(), Is.EqualTo(3));
+    }
+}
