71fbe53c9b991418488d509977caf7added69c2a
Dmitrii Bordukov <dabordukov@mail.ru>
2025-04-26 19:25:33 +0300

    style: remove unused using directive for cleaner code

--- 2st-semester/ExpressionTree/ExpressionTree/Program.cs
+++ 2st-semester/ExpressionTree/ExpressionTree/Program.cs
@@ -3,8 +3,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-using System.Linq.Expressions;
-
 string helpMessage = $"""
 Evaluate expression tree
 
