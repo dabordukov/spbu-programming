19674387486b0df13aa34c9a2129d8546cc8b175
Dmitrii Bordukov <dabordukov@mail.ru>
2025-04-26 19:22:33 +0300

    feat: implement command-line interface for evaluating expression trees

--- 2st-semester/ExpressionTree/ExpressionTree/Program.cs
+++ 2st-semester/ExpressionTree/ExpressionTree/Program.cs
@@ -3,4 +3,29 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-Console.WriteLine("Hello, World!");
+using System.Linq.Expressions;
+
+string helpMessage = $"""
+Evaluate expression tree
+
+    Usage: {System.AppDomain.CurrentDomain.FriendlyName} [INPUT FILE]
+""";
+
+if (args.Length != 1)
+{
+    Console.WriteLine(helpMessage);
+    return 1;
+}
+
+if (!File.Exists(args[0]))
+{
+    Console.WriteLine($"File not found: {args[0]}");
+    return 2;
+}
+
+var tree = new ExpressionTree.ExpressionTree(args[0]);
+Console.WriteLine("Expression tree:");
+tree.Print();
+Console.WriteLine($"Result: {tree.Evaluate()}");
+
+return 0;
