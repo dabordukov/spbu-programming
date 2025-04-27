75da8b118461495bae3a213cc7c1fed15345eae9
Dmitrii Bordukov <dabordukov@mail.ru>
2025-04-27 19:01:17 +0300

    feat: add Filter and Fold methods to Functions class

--- 2st-semester/Functions/Functions/Functions.cs
+++ 2st-semester/Functions/Functions/Functions.cs
@@ -17,4 +17,33 @@ public class Functions
     {
         return Map<T, T>(list, function);
     }
+
+    public static List<T> Filter<T>(List<T> list, Func<T, bool> filter)
+    {
+        var newList = new List<T>();
+        foreach (var element in list)
+        {
+            if (filter(element))
+            {
+                newList.Add(element);
+            }
+        }
+
+        return newList;
+    }
+
+    public static U Fold<T, U>(List<T> list, U accumulator, Func<U, T, U> function)
+    {
+        foreach (var element in list)
+        {
+            accumulator = function(accumulator, element);
+        }
+
+        return accumulator;
+    }
+
+    public static T Fold<T>(List<T> list, T accumulator, Func<T, T, T> function)
+    {
+        return Fold<T, T>(list, accumulator, function);
+    }
 }
