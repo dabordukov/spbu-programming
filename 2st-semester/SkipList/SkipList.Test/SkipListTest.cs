c87cf18853bf7127dc042bafd47248b30eeebdf7
Dmitrii Bordukov <dabordukov@mail.ru>
2025-05-19 12:49:33 +0300

    feat: add unit tests for SkipList and update project configuration

---
+++ 2st-semester/SkipList/SkipList.Test/SkipListTest.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="SkipListTest.cs" company="dabordukov">
+// Copyright (c) dabordukov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace SkipList.Test;
+
+public class SkipListTest
+{
+    private readonly int[] test = [5, 2, 6, 12, -1, 6];
+
+    [Test]
+    public void SkipListStoresElementsInAscendingOrder()
+    {
+        var list = new SkipList<int>();
+        int[] expected = new int[this.test.Length];
+        Array.Copy(this.test, expected, this.test.Length);
+        Array.Sort(expected);
+
+        foreach (var x in this.test)
+        {
+            list.Add(x);
+        }
+
+        Assert.That(Enumerable.SequenceEqual(list, expected), Is.True);
+    }
+}
