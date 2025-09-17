3f59bff6849954d2ab9aa56de313df654e2c42fd
Dmitrii Bordukov <dabordukov@mail.ru>
2025-09-17 15:35:02 +0300

    changed project type to a library

--- 3rd-semester/ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// <copyright file="Program.cs" company="dabordukov">
-// Copyright (c) dabordukov. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-// </copyright>
-
-Console.WriteLine("Hello, World!");
