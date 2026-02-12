9abafd409b43624bb61800ef3a34da7ec489eff0
Dmitrii Bordukov <dabordukov@mail.ru>
2026-02-13 02:58:18 +0300

    test: add tests for reverseList function

---
+++ 4th-semester/ListReverseHW/ListReverse.Test/UnitTest1.fs
@@ -0,0 +1,16 @@
+﻿module ListReverse.Test
+
+open NUnit.Framework
+open FsUnit
+open ListReverse.ListReverse
+
+[<Test>]
+let ``reverseList function must reverse list correctly`` () =
+    reverseList [ 1; 2; 3; 4 ] |> should equal [ 4; 3; 2; 1 ]
+
+[<Test>]
+let ``reverseList function must return the same list on list of single element`` () =
+    reverseList [ 1 ] |> should equal [ 1 ]
+
+[<Test>]
+let ``reverseList function must return empty list on empty list`` () = reverseList [] |> should equal []
