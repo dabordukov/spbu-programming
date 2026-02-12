80b923dd09a044cf78df314ee508df6b9f170d1d
Dmitrii Bordukov <dabordukov@mail.ru>
2026-02-12 16:10:19 +0300

    add SeriesOfPowersHW

---
+++ 4th-semester/SeriesOfPowersHW/SeriesOfPowersHW/PowerOfSeries.fs
@@ -0,0 +1,31 @@
+﻿// <copyright file="PowerOfSeries.fs" company="dabordukov">
+// Copyright (c) dabordukov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+open System
+
+// Generate [2^n; 2^(n + 1); ...; 2^(n + m)] list
+let getListOfPowersOfTwo n m =
+    let rec calculateFirstPower power acc =
+        if power = 0 then
+            acc
+        else if power < 0 then
+            calculateFirstPower (power + 1) (acc / 2.0)
+        else
+            calculateFirstPower (power - 1) (acc * 2.0)
+
+    let rec generateList list counter =
+        if counter = 0 then
+            list
+        else
+            generateList (2.0 * List.head list :: list) (counter - 1)
+
+    List.rev (generateList [ calculateFirstPower n 1.0 ] m)
+
+let args = Environment.GetCommandLineArgs()
+
+if args.Length = 3 then
+    printfn "%A" (getListOfPowersOfTwo (int args.[1]) (int args.[2]))
+else
+    printfn "usage: ./bin n m\nn, m - integer numbers"
