3d9e314d6954ae9630bbb096884950a5fb58455d
Dmitrii Bordukov <dabordukov@mail.ru>
2026-02-14 13:36:47 +0300

    Fibonacci hw

---
+++ 4th-semester/FibonacciHW/Fibonacci.Test/FibonacciTests.fs
@@ -0,0 +1,16 @@
+﻿module Fibonacci.Test
+
+open NUnit.Framework
+open FsUnit
+open Fibonacci
+
+[<TestCase(0, 0)>]
+[<TestCase(1, 1)>]
+[<TestCase(2, 1)>]
+[<TestCase(8, 21)>]
+[<TestCase(19, 4181)>]
+let ``Should correctly calculate n-th Fibonacci number`` n expected =
+    fibonacci n |> should equal (Some expected)
+
+[<Test>]
+let ``Should return None for n<0`` () = fibonacci -2 |> should equal None
